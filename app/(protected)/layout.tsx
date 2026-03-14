719a8209eeaabe82dbc24b4033fce0618f8c0cd9
Baba <baba@users.noreply.github.com>
2026-03-14 20:40:00 +0100

    layout(print): hide sidebar/navbar on print and expand content; improve print readability for lab PDFs

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -6,15 +6,17 @@ export const dynamic = "force-dynamic";
 
 const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="w-full h-screen flex bg-gray-200">
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
+    <div className="w-full h-screen flex bg-gray-200 print:block">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] print:hidden">
         <Sidebar />
       </div>
 
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] flex flex-col">
-        <Navbar />
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] flex flex-col print:w-full">
+        <div className="print:hidden">
+          <Navbar />
+        </div>
 
-        <div className="h-full w-full p-2 overflow-y-scroll">{children}</div>
+        <div className="h-full w-full p-2 overflow-y-scroll print:overflow-visible print:p-0">{children}</div>
       </div>
     </div>
   );
